5e91873f50b7fe0fd66f093b27a89b7bba280753
nusratnova5 <nusrat.nova16@gmail.com>
2022-11-09 21:54:23 +0600

    client-site environment variable added

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,8 +1,23 @@
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
     {
         path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            }
+        ]
     }
 ])
 
